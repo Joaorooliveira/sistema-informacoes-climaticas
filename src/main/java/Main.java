b5499e9dbfba26f2e120f4d9b1f3be324e91c909
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-29 14:25:53 -0300

    feat: adiciona metodo para imprimir os dados climaticos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
 import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
@@ -40,5 +42,39 @@ public class Main {
         //Agora vamos enviar requisicoes HTTP e recebber respostas HTTP, comunicar com o site da API Metereologica
         HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
         return response.body(); // retorna os dados metereologicos obtidos no site API (WeatherAPI)
+    }
+
+    //Metodo para imprimir os dados metereologicos de forma organizada
+    public static void imprimirDadosClimaticos(String dados){
+        //System.out.println("Dados originais (JSON) obtidos no site metereologico"+dados);
+
+        JSONPObject dadosJson = new JSONPObject(dados);
+        JSONPObject informacoesMetereologicas = dadosJson.getJSONObject("current");
+
+        //Extrai os dados da localizacao
+        String cidade = dadosJson.getJSONObject("location").getString("name");
+        String pais = dadosJson.getJSONObject("location").getString("country");
+
+        //Extrai os dados adicionais
+        String condicaoTempo = informacoesMetereologicas.getJSONObject("condition").getString("text");
+        int umidade = informacoesMetereologicas.getInt("humidity");
+        float velocidadeVento = informacoesMetereologicas.getFloat("wind_kph");
+        float pressaoAtmosferica = informacoesMetereologicas.getFloat("pressure_mb");
+        float sensacaoTermica = informacoesMetereologicas.getFloat("feelslike_c");
+        float temperaturaAtual = informacoesMetereologicas.getFloat("temp_c");
+
+        //Extrai a data e hora da string retornada pela API
+        String dataHoraString = informacoesMetereologicas.getString("last_update");
+
+        //Imprime as informacoes atuais
+        System.out.println("Informacoes Metereologicas para "+ cidade + ", "+ pais);
+        System.out.println("Data e hora : " + dataHoraString);
+        System.out.println("Temperatura atual : " + temperaturaAtual+"C");
+        System.out.println("Sensacao Termica : " + sensacaoTermica+"C");
+        System.out.println("Condicao do tempo: " + condicaoTempo);
+        System.out.println("Umidade: "+umidade+"%");
+        System.out.println("Velocidade do Vento: " + velocidadeVento+" km/h");
+        System.out.println("Pressao Atmosferica: " + pressaoAtmosferica);
+
     }
 }
