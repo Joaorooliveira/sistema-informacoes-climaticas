17ea0a5a13f43407380065a468614a05dc843563
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-29 14:09:43 -0300

    feat: adiciona metodo para receber os dados climaticos da api

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
@@ -18,5 +25,20 @@ public class Main {
         }catch(Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+    public static String getDadosClimaticos(String cidade) throws Exception{
+        String apiKey = Files.readString(Paths.get("/resources/api-Key.txt")).trim();
+
+        String formataNomeCidade = URLEncoder.encode(cidade, "UTF-8");
+        String apiUrl = "http://api.weatherapi.com/v1/current.json?key="+ apiKey+"&q="+formataNomeCidade;
+        HttpRequest request = HttpRequest.newBuilder() // Comeca a construcao de uma nova solicitacao HTTP
+                .uri(URI.create(apiUrl))
+                .build(); //Finaliza a construcao da solicitacao HTTP.
+        //Criar objeto enviar solicitacoes HTTP e receber respostas HTTP, para acessar o site da WeatherAPI
+        HttpClient client = HttpClient.newHttpClient();
+
+        //Agora vamos enviar requisicoes HTTP e recebber respostas HTTP, comunicar com o site da API Metereologica
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+        return response.body(); // retorna os dados metereologicos obtidos no site API (WeatherAPI)
     }
 }
